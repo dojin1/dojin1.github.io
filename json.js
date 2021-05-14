0e24c1c38190de664363aa7f020059f075e45d8f
dojin <dojin@daum.net>
2021-05-14 15:41:20 +0900

    6교시 커밋

---
+++ json.js
@@ -0,0 +1,122 @@
+var jsonData = [
+    {
+      "_id": "609e125e1523f101513bb947",
+      "age": 25,
+      "eyeColor": "brown",
+      "name": "Strong Gilliam"
+    },
+    {
+      "_id": "609e125e183329df4b2dfceb",
+      "age": 29,
+      "eyeColor": "brown",
+      "name": "Alberta Chandler"
+    },
+    {
+      "_id": "609e125ef78ff7c35ebca672",
+      "age": 25,
+      "eyeColor": "blue",
+      "name": "Marsh Cantu"
+    },
+    {
+      "_id": "609e125e8b874ff081d8bf29",
+      "age": 29,
+      "eyeColor": "green",
+      "name": "Betty Lowe"
+    },
+    {
+      "_id": "609e125e05c379398b818f90",
+      "age": 26,
+      "eyeColor": "brown",
+      "name": "Annie Barr"
+    },
+    {
+      "_id": "609e125ed636bff85a4972da",
+      "age": 27,
+      "eyeColor": "brown",
+      "name": "Deidre Boyer"
+    },
+    {
+      "_id": "609e125e1e5a1d7dc38b3def",
+      "age": 37,
+      "eyeColor": "green",
+      "name": "Curtis English"
+    },
+    {
+      "_id": "609e125ee415a60113327ad0",
+      "age": 40,
+      "eyeColor": "blue",
+      "name": "Lea Pratt"
+    },
+    {
+      "_id": "609e125e2d48ebd972c8ebef",
+      "age": 37,
+      "eyeColor": "blue",
+      "name": "Angeline Mckay"
+    },
+    {
+      "_id": "609e125e60f48e811c889622",
+      "age": 24,
+      "eyeColor": "brown",
+      "name": "Jami Grimes"
+    },
+    {
+      "_id": "609e125e566188ebe921a1de",
+      "age": 32,
+      "eyeColor": "blue",
+      "name": "Burris Velazquez"
+    },
+    {
+      "_id": "609e125e415b79ae7126e014",
+      "age": 31,
+      "eyeColor": "blue",
+      "name": "Bridget Hampton"
+    },
+    {
+      "_id": "609e125e40b6752acd5f13af",
+      "age": 35,
+      "eyeColor": "green",
+      "name": "Noelle Buck"
+    },
+    {
+      "_id": "609e125e02c08e8fd7a14d09",
+      "age": 22,
+      "eyeColor": "blue",
+      "name": "Holt Richard"
+    },
+    {
+      "_id": "609e125e25d24b2d60707899",
+      "age": 30,
+      "eyeColor": "green",
+      "name": "Porter Benjamin"
+    },
+    {
+      "_id": "609e125e4d089f0f9b8d98f3",
+      "age": 40,
+      "eyeColor": "blue",
+      "name": "Hardy Noel"
+    },
+    {
+      "_id": "609e125ec94a313e0c4b7882",
+      "age": 37,
+      "eyeColor": "blue",
+      "name": "Terrie Luna"
+    },
+    {
+      "_id": "609e125e35eb6b3eece957c3",
+      "age": 28,
+      "eyeColor": "green",
+      "name": "Brooks George"
+    },
+    {
+      "_id": "609e125edcb811855e2f2bbf",
+      "age": 29,
+      "eyeColor": "brown",
+      "name": "Lois Burgess"
+    },
+    {
+      "_id": "609e125ef7cb5b44d57c5d04",
+      "age": 20,
+      "eyeColor": "green",
+      "name": "Jensen Hull"
+    }
+  ]
